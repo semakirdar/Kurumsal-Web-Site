0704cb151cab7c554940925230eab250a76a49ac
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-05 15:36:42 +0300

    MVC

---
+++ KurumsalWeb/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using KurumsalWeb.Models.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KurumsalWeb.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        private KurumsalDBContext db = new KurumsalDBContext();
+        public ActionResult Index()
+        {
+            return View(db.Blog.ToList());
+        }
+    }
+}
